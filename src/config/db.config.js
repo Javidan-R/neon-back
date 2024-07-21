467591536eaad459b3d057d5ffd02cc48b0e23b0
Javidan-R <rajablijavidan@gmail.com>
2024-07-20 19:11:21 -0700

    Back end deploying

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,12 +1,14 @@
-require('dotenv').config(); 
-
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  port: process.env.DB_PORT,
-  logging: false, 
+  protocol: 'postgres',
+  logging: false,
 });
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.error('Unable to connect to the database:', err));
 
 module.exports = sequelize;
