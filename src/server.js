3f28d31cc75c121044ce9eccb3e422da5581f71b
Javidan-R <rajablijavidan@gmail.com>
2024-07-20 19:29:51 -0700

    Add Procfile for deployment

--- src/server.js
+++ src/server.js
@@ -1,36 +1,32 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
-const apiRoutes = require('./routes/api.routes');
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.use(cors());
-app.use(express.json());
-app.use('/api', apiRoutes);
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
-const express = require('express');
+const path = require('path');
 const sequelize = require('./config/db.config');
 const userRoutes = require('./routes/user.routes');
 const bonusRoutes = require('./routes/bonus.routes');
 const qrCodeRoutes = require('./routes/qrCode.routes');
-const cors = require('cors');
+const apiRoutes = require('./routes/api.routes');
 
 const app = express();
-const port = 5002;
+const port = process.env.PORT || 5002; // PORT ətraf mühit dəyişəni ilə əvəzlənə bilər
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
+// API Routes
 app.use('/api', userRoutes);
 app.use('/api', bonusRoutes);
 app.use('/api', qrCodeRoutes);
+app.use('/api', apiRoutes);
 
+// Serve React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Database connection and sync
 sequelize.authenticate()
   .then(() => {
     console.log('Connection to the database has been established successfully.');
@@ -47,6 +43,7 @@ sequelize.sync({ force: false })
     console.error('Error syncing database:', err);
   });
 
+// Start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
